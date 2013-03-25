3d4a423a9c7a539dd0309833067fdd944f1a6250
Tomas Løkken <tomas.lokken@gmail.com>
2013-03-25 14:02:16 +0100

    Ignore config.php

--- html/libs/config.php
+++ html/libs/config.php
@@ -35,7 +35,7 @@ $webim_encoding = "utf-8";
 $mysqlhost = "localhost";
 $mysqldb = "webim";
 $mysqllogin = "webim";
-$mysqlpass = "ikke lagre noe passord på git";
+$mysqlpass = "y7TBQe7JmXFCKjf3";
 $mysqlprefix = "";
 
 $dbencoding = "utf8";
