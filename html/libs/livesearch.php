2d4feb30e17a109bc34909ff465427424f26bade
Tomas Løkken <tomas.lokken@gmail.com>
2013-04-02 15:26:05 +0200

    Kommentert og ryddet opp.

--- html/libs/livesearch.php
+++ html/libs/livesearch.php
@@ -1,21 +1,21 @@
 <?php
-//$parser = xml_parser_create();
-
 $xml = simplexml_load_file('../data/ldap_users.xml');
 
+// Mottar tekst det skal søkes etter fra jQuery
 $str = $_GET['term'];
 
-//if (strlen($str) > 1) {
-	$unameList = "";
-	
-	$unames = $xml->xpath("/tromkom/user/username[contains(text(), '$str')]");
-	foreach ($unames as $user) {
-		$unameList = $unameList . "," . $user;
-	}
-	
-	$unameList = trim($unameList, ",");
-	$unameList = explode(",", $unameList);
-	echo json_encode($unameList);	
-	//}
+$unameList = "";
+
+// Lager en CSV med brukernavn
+$unames = $xml->xpath("/tromkom/user/username[contains(text(), '$str')]");
+foreach ($unames as $user) {
+	$unameList = $unameList . "," . $user;
+}
+
+// Fjerner første komma
+$unameList = trim($unameList, ",");
+// Lager et array av teksten
+$unameList = explode(",", $unameList);
+echo json_encode($unameList);	
 
 ?>
